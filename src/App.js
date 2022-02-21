5acd5c1cd3b7736cfb069facb1673b81e849ad0a
tuanit168 <tuanit168@gmail.com>
2022-02-21 16:34:13 +0700

    Event binding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.handleClick2 = this.handleClick1.bind(this);
+  }
+
+  handleClick1() {
+    console.log(this);
+  }
+
+  handleClick3 = () => console.log(this);
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick1()}>Click 1</button>
+        <button onClick={this.handleClick1}>Click 2</button>
+        <button onClick={this.handleClick2}>Click 3</button>
+        <button onClick={this.handleClick3}>Click 4</button>
+      </div>
+    );
+  }
 }
 
 export default App;
