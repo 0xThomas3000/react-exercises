8ef80035d1fc6350db261f998857ca00c154c035
tuanit168 <tuanit168@gmail.com>
2022-02-25 17:44:31 +0700

    Some demo for react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
-import logo from './logo.svg';
-import React, {Component} from 'react';
+import React from 'react';
+import {Routes, Route, useParams, Link, useNavigate} from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  state = {
-    meaningOfLife: 47,
-  }
+const HomePage = () => {
+  const navigate = useNavigate();
 
-  handleClick = () => {
-    this.setState((prevState, prevProps) => {
-        return {meaningOfLife: prevState.meaningOfLife + 1};
-      }, () => {
-        console.log(this.state.meaningOfLife);
-      }
-    );
-  }
+  return (
+    <div>
+      <Link to='topics'>Topics</Link>
+      <br/><br/>
+      <button onClick={() => navigate('topics')}>Topics</button>
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.meaningOfLife}
-          </p>
-          <button onClick={this.handleClick}>
-            Update State
-          </button>
-        </header>
-      </div>
-    );
-  }
+const TopicsList = () => {
+  return (
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+      <Link to={`13`}>Topic 13</Link><br/>
+      <Link to={`17`}>Topic 17</Link><br/>
+      <Link to={`21`}>Topic 21</Link>
+    </div>
+  );
+};
+
+const TopicDetail = () => {
+  let {topicId} = useParams();
+
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE: {topicId}</h1>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='topics' element={<TopicsList />} />
+        <Route path='topics/:topicId' element={<TopicDetail />} />
+        <Route path='blog/kkk/topics' element={<TopicsList />} />
+        <Route path='blog/kkk/topics/:topicId' element={<TopicDetail />} />
+        <Route path='blog/topics' element={<TopicsList />} />
+        <Route path='blog/topics/:topicId' element={<TopicDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
